37ae06364d1ebe5135e9708dc6fe0a85476feb4f
Jeff Huntsman <jeffreydhuntsman@gmail.com>
2015-08-14 13:26:24 -0400

    renamed package nuspec

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -29,4 +29,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.50.6.*")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyInformationalVersion("0.50.6-alpha1")]
